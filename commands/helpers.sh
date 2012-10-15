eb4c4593c5e210b2e1dee689dcebc1c1dd7e2cf7
Pedro Lacerda <kanvuanza@gmail.com>
2012-10-15 03:19:17 -0300

    agora options() é silencioso

--- commands/helpers.sh
+++ commands/helpers.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 
 usage() {
     sed -n '0,/# -\+$/d;/# -\+$/,$d;s/^#\s\?//g;p' "$0" >&2
@@ -9,9 +9,6 @@ die() {
     exit 1
 }
 
-
 options() {
-    TEMP=`getopt -o "$1" -l "$2" -n "$0" -- "${@:3:$#}"`
-    [[ $? != 0 ]] && die "getopt malformado"
-    echo "$TEMP"
+    getopt -q -n "$0" -o "$1" -l "$2" -- "${@:3:$#}"
 }
